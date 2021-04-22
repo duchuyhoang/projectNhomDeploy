4438279344a7ee2504fd90b88e97094f1fa2424c
Hồng Hải Nguyễn <honghainguyen2000@gmail.com>
2021-04-22 12:41:40 +0700

    add loading component

---
+++ src/components/shared/CNLoading/CNLoading.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CNLoading.css";
+
+export const Loading = () =>{
+    return (
+        <div class="loading-screen">
+            <div class="loading">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {AppWithContexts} from "./AppWithContexts";
+// import {Loading} from "@Components/shared/CNLoading/CNLoading"
 
 
 ReactDOM.render(<AppWithContexts />,document.getElementById("root"));
