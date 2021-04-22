3a185b697c2cc77cc4c039097774eaceff29480b
Duc Huy Hoang <huyhoang10032000@gmail.com>
2021-04-22 11:43:20 +0700

    add router and lazy loading

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import "./App.css";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from "react-router-dom";
+const Message=lazy(()=>import("@Components/Message"));
 
-export const App=({title})=>{
-return (
-    <h1 className="helloWorld">{title}</h1>
-)
+
+export const App = ({ title }) => {
+    return (
+        <Router>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                    <Route path="/home">
+                        <Message />
+                    </Route>
+                    <Route path="/users">
+                    </Route>
+                    <Route path="/">
+                        <Redirect to={{
+                            pathname:"/home"
+                        }} />
+                    </Route>
+                </Switch>
+
+            </Suspense>
+
+
+        </Router>
+    )
 }
--- src/AppWithContexts.js
+++ src/AppWithContexts.js
@@ -3,16 +3,18 @@ import {App} from "./App"
 import {Message} from "@Components/Message";
 import { ThemeProvider as MaterialThemeProvider } from '@material-ui/core'
 import theme from "./theme";
+import {ErrorBoundary} from "@Components/ErrorBoundary/ErrorBoundary";
 export const AppWithContexts=()=>{
 // Some context below return
 return (
 <>
 <MaterialThemeProvider theme={theme}>
-
+<ErrorBoundary>
 <App title="Hello world"/>
-<Message message="View web pack config for this example import @Component" />
-
+{/* <Message message="View web pack config for this example import @Component" /> */}
+</ErrorBoundary>
 </MaterialThemeProvider>
+
 </>
 )
 
