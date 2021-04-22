9c3f3098647d4b4eb5a7cb91d9d2cb12b5e71756
Hồng Hải Nguyễn <honghainguyen2000@gmail.com>
2021-04-22 21:23:09 +0700

    fix css loading..

--- src/components/shared/CNLoading/CNLoading.js
+++ src/components/shared/CNLoading/CNLoading.js
@@ -3,8 +3,8 @@ import "./CNLoading.css";
 
 export const Loading = () =>{
     return (
-        <div class="loading-screen">
-            <div class="loading">
+        <div className="loading-screen">
+            <div className="loading">
                 <span></span>
                 <span></span>
                 <span></span>
