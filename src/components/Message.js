fbe0bc50cbbc9cf390f7c253c8ef6bbd95ec9951
Duc Huy Hoang <huyhoang10032000@gmail.com>
2021-04-28 19:34:39 +0700

    card component

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,47 +3,64 @@ import { SVGIcon } from "@Components/shared/SvgIcon/Icon";
 import { makeStyles } from '@material-ui/core/styles';
 import { CNSnackBar } from "./shared/CNSnackBar/CNSnackBar";
 import FixedContainer from "@Components/shared/Layout/FixedContainer"
-import { CNButton } from "@Components/shared/CNButton/CNButton"
+import { CNNotifications } from "@Components/shared/CNNotifications/CNNotifications";
+import { CNSelect } from "@Components/shared/CNSelect/CNSelect";
+import { CNCard } from "@Components/shared/CNCard/CNCard";
 
 
-const useStyles = makeStyles(theme => { 
+const useStyles = makeStyles(theme => {
     return {
-        btnStyle: {
-            display: "flex",
-            justifyContent:"center",
-            right:"0",
-            top:"0",
-            bottom:"0",
-            left:"0",
-            position: "fixed", 
-            alignItems: "center",
-            flexDirection:"column",
+        root: (props) => {
+            return {
+                ...theme.typography.header
+            }
+        }
     }
     }      
 })
 
 
- const Message = ({ message }) => {
-    const classes = useStyles();
+const Message = ({ message }) => {
+    const styles = useStyles();
     const [isOpen, setIsOpen] = useState(true);
-    const [selectValue,setSelecteValue]=useState(null);
+    const [selectValue, setSelecteValue] = useState(null);
 
     const handleChange = e => {
-      if(e===null)
-        setSelecteValue(null)
-      else
-      setSelecteValue(e?.value)
-      }
-      
+        if (e === null)
+            setSelecteValue(null)
+        else
+            setSelecteValue(e?.value)
+    }
+
 
     return (
         <>
-            
-            <div className={classes.btnStyle}>
-                <CNButton type="secondary" startIcon={<SVGIcon name="plus" width="12px" height="12px" />}></CNButton>
-                <CNButton type="main"></CNButton>
-            </div>
+            <div style={{ height: "200px", width: "300px" }}>
 
+
+                <CNCard headerComponent={<img src="https://www.elle.vn/wp-content/uploads/2017/07/25/hinh-anh-dep-1.jpg" width="100%" maxHeight="400" />}
+                    bodyComponent={
+                        <>
+                            <p>
+                                Lizard
+          </p>
+                            <p>
+                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                across all continents except Antarctica
+          </p>
+                        </>
+                    }
+
+                    footerComponent={<div style={{ display: "flex", justifyContent: "center" }}>
+                        <button>Hello</button>
+                        <button>Hello11111</button>
+
+                    </div>}
+
+
+                />
+
+            </div>
         </>
     )
 
